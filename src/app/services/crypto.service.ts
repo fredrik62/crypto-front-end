b051e14717cf41f5b2deda0f1fa2d01b9c1619cd
fredrik62 <hogstrom10@gmail.com>
2018-05-16 18:14:48 +0200

    deploying

--- src/app/services/crypto.service.ts
+++ src/app/services/crypto.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http'
 import 'rxjs/add/operator/map';
-import { environment } from '../../environments/environment';
 
 @Injectable()
 export class CryptoService {
